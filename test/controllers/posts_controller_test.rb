ef7d3ee6364de0e9a63bd3c8cd42b0dd0eadf232
Maxim Topciu <mtopciu@adguard.com>
2021-10-04 23:55:40 +0300

    add test on create action

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -3,8 +3,23 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test 'should get index' do
     get posts_url
     assert_response :success
+  end
+
+  test 'should create post' do
+    user = users(:one)
+    post_category = post_categories(:one)
+
+    sign_in user
+
+    assert_difference('Post.count') do
+      post posts_url, params: { post: { title: 'title1', body: 'body1', post_category_id: post_category.id } }
+    end
+
+    assert_redirected_to root_url
   end
 end
