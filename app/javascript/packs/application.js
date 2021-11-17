bfb82575fdcb87058c43cb86fe353a982fa3f685
Maxim Topciu <mtopciu@adguard.com>
2021-11-17 08:03:17 +0300

    fix collapse

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import 'jquery';
-import '@popperjs/core';
 import 'bootstrap';
 
 Rails.start()
