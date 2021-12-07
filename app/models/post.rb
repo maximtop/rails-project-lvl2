8e8105d5e9dc505ddad5e0b35d6d3c16b578568d
Maxim Topciu <mtopciu@adguard.com>
2021-12-07 07:55:31 +0300

    remove redundant inverse of

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :post_category
-  belongs_to :creator, class_name: 'User', inverse_of: :posts
+  belongs_to :creator, class_name: 'User'
 
   has_many :post_comments, dependent: :destroy
   has_many :post_likes, dependent: :destroy
