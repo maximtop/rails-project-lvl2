7bc042e0da23570ab24970d0c45e3c69f516e883
Maxim Topciu <mtopciu@adguard.com>
2021-12-07 08:07:30 +0300

    remove commented lines of code

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class PostComment < ApplicationRecord
-  # self.table_name = 'post_comments'
   has_ancestry
 
   belongs_to :post
--- app/models/post_like.rb
+++ app/models/post_like.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class PostLike < ApplicationRecord
-  # self.table_name = 'post_likes'
   belongs_to :user
   belongs_to :post
 end
